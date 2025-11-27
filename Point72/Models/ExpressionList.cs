3bb29da4d92d602d3d6cb2c5e5bd06bd39b38be1
ImOfficial <himanshu.dubey124@gmail.com>
2025-11-27 20:24:14 +0530

    Adding classes for incoming request for Expression evaluator and fetching list of expression

---
+++ Point72/Models/ExpressionList.cs
@@ -0,0 +1,8 @@
+﻿namespace Point72.Models
+{
+    public class ExpressionList
+    {
+        public String Result { get; set; }
+        public List<String> ExprssionList { get; set; } = String.Empty.Split(',').ToList();
+    }
+}
---
+++ Point72/Models/ExpressionRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Point72.Models
+{
+    public class ExpressionRequest
+    {
+        public String Expression { get; set; }= String.Empty;
+    }
+}
